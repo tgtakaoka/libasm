1d455754c49c52561af53739e4f1151db51e9e5b
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2019-07-19 17:29:46 +0900

    Clean up MC6809 and HD6309 assemblers

--- Makefile
+++ Makefile
@@ -14,16 +14,20 @@ string_utils.o: string_utils.h
 table_mc6809.o: config_mc6809.h entry_mc6809.h insn_table.h text_mc6809.h
 table_hd6309.o: config_hd6309.h entry_mc6809.h text_hd6309.h
 
-asm_mc6809.h: config_hd6309.h error_reporter.h insn.h symbol_table.h table_mc6809.h
+asm.h: insn.h
+asm_mc6809.h: config_hd6309.h asm.h error_reporter.h insn.h symbol_table.h table_mc6809.h
 asm_hd6309.h: config_mc6809.h asm_mc6809.h table_mc6809.h
-config_mc6809.h: config_host.h
+config_mc6809.h: config_host.h error_mc6809.h
 config_hd6309.h: config_host.h
+config_host.h:
+entry_mc6809.h: config_host.h
 error_reporter.h : config_host.h
 insn.h: config_host.h
-entry_mc6809.h: config_host.h
+insn_table.h: config_host.h
 string_utils.h:
 symbol_table.h: config_host.h
-insn_table.h:
+table_mc6809.h: entry_mc6809.h insn_table.h
+table_hd6309.h: table_mc6809.h
 text_mc6809.h: config_host.h
 text_hd6309.h: config_host.h
 
--- asm_hd6309.h
+++ asm_hd6309.h
@@ -9,18 +9,28 @@
 
 class AsmHd6309 : public AsmMc6809 {
 public:
-    Error encode(const char *line, Insn &insn,
+    virtual Error encode(const char *line, Insn &insn,
                  target::uintptr_t addr, SymbolTable *symtab) override;
 
 protected:
     static TableHd6309 _tableHd6309;
 
-    Error getOperand32(const char *&line, target::uint32_t &val);
-    Error encodeImmediate(const char *line, Insn &insn) override;
-    Error encodeIndexed(const char *line, Insn &insn) override;
+    virtual host::int_t encodeRegister(RegName regName) const override;
+    virtual RegName parseRegister(const char *line) const override;
+    virtual host::int_t encodeIndexReg(RegName regName) const override;
+    virtual RegName parseIndexReg(const char *line) const override;
+    virtual host::int_t encodeBaseReg(RegName regName) const override;
+    virtual RegName parseBaseReg(const char *line) const override;
+
+    Error getOperand32(const char *&line, target::uint32_t &val) const;
     Error encodeBitOperation(const char *line, Insn &insn);
     Error encodeImmediatePlus(const char *line, Insn &insn);
     Error encodeTransferMemory(const char *line, Insn &insn);
+
+    virtual Error encodeImmediateExtra(const char *line, Insn &insn) override;
+    virtual Error encodeIndexedExtra(
+        Insn &insn, RegName index, target::uintptr_t addr,
+        RegName base, host::int_t incr, bool indir) override;
 };
 
 #endif // __ASM_HD6309_H__
