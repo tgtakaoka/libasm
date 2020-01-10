c949572f246bc753513ffb621809b54eea2c96d7
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-01-10 11:40:50 +0900

    [cli] Make command line option a bit generic

--- cli/asm_driver.h
+++ cli/asm_driver.h
@@ -165,51 +165,64 @@ private:
         _input_name = nullptr;
         _output_name = nullptr;
         _list_name = nullptr;
-        bool opt_list = false;
+        _formatter = nullptr;
+        char formatter = 0;
         for (int i = 1; i < argc; i++) {
             const char *opt = argv[i];
             if (*opt == '-') {
                 if (strcmp(opt, "-o") == 0) {
-                    if (++i >= argc) return 1;
-                    const char *fmt = basename(argv[i], '.');
-                    if (strcasecmp(fmt, "hex") == 0) {
-                        _formatter = new IntelHex<Addr>();
-                    } else if (strcasecmp(fmt, "s19") == 0) {
-                        _formatter = new SRecord<Addr>();
-                    } else {
-                        fprintf(stderr, "Unknown output format: %s\n", fmt);
+                    if (++i >= argc) {
+                        fprintf(stderr, "-o requires output file name\n");
                         return 1;
                     }
                     _output_name = argv[i];
                 } else if (strcmp(opt, "-l") == 0) {
-                    opt_list = true;
+                    if (++i >= argc) {
+                        fprintf(stderr, "-l requires listing file name\n");
+                        return 1;
+                    }
+                    _list_name = argv[i];
+                } else if (strcmp(opt, "-S") == 0
+                           || strcmp(opt, "-H") == 0) {
+                    formatter = opt[1];
                 } else {
+                    fprintf(stderr, "unknown option: %s\n", opt);
                     return 1;
                 }
             } else {
-                if (_input_name) return 1;
+                if (_input_name) {
+                    fprintf(stderr,
+                            "multiple input files specified: %s and %s\n",
+                            _input_name, opt);
+                    return 1;
+                }
                 _input_name = opt;
             }
         }
-        if (!_input_name) return 1;
-        if (opt_list) {
-            char *name = static_cast<char *>(
-                malloc(strlen(_input_name) + 5));
-            strcpy(name, _input_name);
-            char *suffix = strrchr(name, '.');
-            if (!suffix) suffix = name + strlen(name);
-            strcpy(suffix, ".lst");
-            _list_name = name;
+        if (!_input_name) {
+            fprintf(stderr, "no input file\n");
+            return 1;
+        }
+        if (_output_name && strcmp(_output_name, _input_name) == 0) {
+            fprintf(stderr, "output file overwrite input file\n");
+            return 2;
+        }
+        if (_list_name && strcmp(_list_name, _input_name) == 0) {
+            fprintf(stderr, "listing file overwrite input file\n");
+            return 2;
+        }
+        if (_output_name) {
+            if (formatter == 'S') _formatter = new SRecord<Addr>();
+            if (formatter == 'H') _formatter = new IntelHex<Addr>();
         }
         return 0;
     }
 
     int usage() {
         fprintf(stderr,
-                "usage: %s [-o <output>.<fmt>] [-l] <input>\n"
-                "  <fmt> can be\n"
-                "        hex Intel Hex\n"
-                "        s19 Motorola S-record\n",
+                "usage: %s [-SH] [-o <output>] [-l <list>] <input>\n"
+                "  -S : output Motorola SREC format\n"
+                "  -H : output Intel HEX format\n",
                 _progname);
         return 2;
     }
