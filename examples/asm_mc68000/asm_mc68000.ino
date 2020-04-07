23da46d4f5e451d7417edc9df73b26ae214f6330
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-04-07 19:19:14 +0900

    [Arduino] add MC68000 assembler/disassembler examples

---
+++ examples/asm_mc68000/asm_mc68000.ino
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 Tadashi G. Takaoka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <asm_mc68000.h>
+#include <libcli.h>
+
+using namespace libasm;
+using namespace libasm::mc68000;
+
+AsmMc68000 as68k;
+Assembler<target::uintptr_t> &assembler(as68k);
+
+void assemble(const char *line) {
+  Insn insn;
+  if (assembler.encode(line, insn, 0x10000, nullptr)) {
+    Cli.print(F("Error "));
+    Cli.print(assembler.getError());
+    Cli.print(F(" at: "));
+    Cli.println(assembler.errorAt());
+  } else {
+    Cli.printUint24(insn.address());
+    Cli.print(':');
+    for (int i = 0; i < insn.length(); i += 2) {
+      Cli.print(' ');
+      uint16_t val = static_cast<uint16_t>(insn.bytes()[i]) << 8;
+      val |= insn.bytes()[i+1];
+      Cli.printUint16(val);
+    }
+    Cli.println();
+  }
+}
+
+bool handleLine(Cli::State state, char *line, uintptr_t extra) {
+  assemble(line);
+  return Cli.readLine(handleLine, 0);
+}
+
+void setup() {
+  assembler.setCpu("68008");
+
+  Serial.begin(9600);
+  Cli.begin(Serial);
+  Cli.readLine(handleLine, 0);
+}
+
+void loop() {
+  Cli.loop();
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// tab-width: 2
+// End:
+// vim: set ft=cpp et ts=2 sw=2:
---
+++ examples/dis_mc68000/dis_mc68000.ino
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 Tadashi G. Takaoka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dis_mc68000.h>
+#include <str_memory.h>
+#include <libcli.h>
+
+using namespace libasm;
+using namespace libasm::mc68000;
+
+DisMc68000 dis68k;
+Disassembler<target::uintptr_t> &disassembler(dis68k);
+
+void disassemble(DisMemory<target::uintptr_t> &memory) {
+  char operands[20];
+  Insn insn;
+  while (memory.hasNext()) {
+    if (disassembler.decode(memory, insn, operands, nullptr)) {
+      Cli.print(F("Error "));
+      Cli.println(disassembler.getError());
+    } else {
+      Cli.printUint24(insn.address());
+      Cli.print(':');
+      for (int i = 0; i < insn.length(); i += 2) {
+        Cli.print(' ');
+        uint16_t val = static_cast<uint16_t>(insn.bytes()[i]) << 8;
+        val |= insn.bytes()[i+1];
+        Cli.printUint16(val);
+      }
+      for (int i = insn.length(); i < disassembler.maxBytes(); i += 2)
+        Cli.print(F("     "));
+      Cli.print(' ');
+      for (size_t n = Cli.print(insn.name());
+           n <= disassembler.maxName(); n++)
+        Cli.print(' ');
+      Cli.println(operands);
+    }
+  }
+}
+
+bool handleLine(Cli::State state, char *line, uintptr_t extra) {
+  StrMemory memory(0x10000, line);
+  disassemble(memory);
+  return Cli.readLine(handleLine, 0);
+}
+
+void setup() {
+  disassembler.setCpu("68008");
+
+  Serial.begin(9800);
+  Cli.begin(Serial);
+  Cli.readLine(handleLine, 0);
+}
+
+void loop() {
+  Cli.loop();
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// tab-width: 2
+// End:
+// vim: set ft=cpp et ts=2 sw=2:
