9e10c75695b623a04882e3001a3201fa4702a6b3
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-04-13 19:36:03 +0900

    [6800/DIS] Disassembler output MC6800 operands

--- test/gen_mc6800.asm
+++ test/gen_mc6800.asm
@@ -91,110 +91,110 @@
       TST >$007E
       JMP >$007F
       CLR >$0080
-      SUB A,#$81
-      CMP A,#$82
-      SBC A,#$83
-      AND A,#$85
-      BIT A,#$86
-      LDA A,#$87
-      EOR A,#$89
-      ADC A,#$8A
-      ORA A,#$8B
-      ADD A,#$8C
+      SUB A #$81
+      CMP A #$82
+      SBC A #$83
+      AND A #$85
+      BIT A #$86
+      LDA A #$87
+      EOR A #$89
+      ADC A #$8A
+      ORA A #$8B
+      ADD A #$8C
       CPX #$8D
       BSR $0034
       LDS #$8F
-      SUB A,$91
-      CMP A,$92
-      SBC A,$93
-      AND A,$95
-      BIT A,$96
-      LDA A,$97
-      STA A,$98
-      EOR A,$99
-      ADC A,$9A
-      ORA A,$9B
-      ADD A,$9C
+      SUB A $91
+      CMP A $92
+      SBC A $93
+      AND A $95
+      BIT A $96
+      LDA A $97
+      STA A $98
+      EOR A $99
+      ADC A $9A
+      ORA A $9B
+      ADD A $9C
       CPX $9D
       LDS $9F
       STS $A0
-      SUB A,161,X
-      CMP A,162,X
-      SBC A,163,X
-      AND A,165,X
-      BIT A,166,X
-      LDA A,167,X
-      STA A,168,X
-      EOR A,169,X
-      ADC A,170,X
-      ORA A,171,X
-      ADD A,172,X
+      SUB A 161,X
+      CMP A 162,X
+      SBC A 163,X
+      AND A 165,X
+      BIT A 166,X
+      LDA A 167,X
+      STA A 168,X
+      EOR A 169,X
+      ADC A 170,X
+      ORA A 171,X
+      ADD A 172,X
       CPX 173,X
       JSR 174,X
       LDS 175,X
       STS 176,X
-      SUB A,>$00B1
-      CMP A,>$00B2
-      SBC A,>$00B3
-      AND A,>$00B5
-      BIT A,>$00B6
-      LDA A,>$00B7
-      STA A,>$00B8
-      EOR A,>$00B9
-      ADC A,>$00BA
-      ORA A,>$00BB
-      ADD A,>$00BC
+      SUB A >$00B1
+      CMP A >$00B2
+      SBC A >$00B3
+      AND A >$00B5
+      BIT A >$00B6
+      LDA A >$00B7
+      STA A >$00B8
+      EOR A >$00B9
+      ADC A >$00BA
+      ORA A >$00BB
+      ADD A >$00BC
       CPX >$00BD
       JSR >$00BE
       LDS >$00BF
       STS >$00C0
-      SUB B,#$C1
-      CMP B,#$C2
-      SBC B,#$C3
-      AND B,#$C5
-      BIT B,#$C6
-      LDA B,#$C7
-      EOR B,#$C9
-      ADC B,#$CA
-      ORA B,#$CB
-      ADD B,#$CC
+      SUB B #$C1
+      CMP B #$C2
+      SBC B #$C3
+      AND B #$C5
+      BIT B #$C6
+      LDA B #$C7
+      EOR B #$C9
+      ADC B #$CA
+      ORA B #$CB
+      ADD B #$CC
       LDX #$CF
-      SUB B,$D1
-      CMP B,$D2
-      SBC B,$D3
-      AND B,$D5
-      BIT B,$D6
-      LDA B,$D7
-      STA B,$D8
-      EOR B,$D9
-      ADC B,$DA
-      ORA B,$DB
-      ADD B,$DC
+      SUB B $D1
+      CMP B $D2
+      SBC B $D3
+      AND B $D5
+      BIT B $D6
+      LDA B $D7
+      STA B $D8
+      EOR B $D9
+      ADC B $DA
+      ORA B $DB
+      ADD B $DC
       LDX $DF
       STX $E0
-      SUB B,225,X
-      CMP B,226,X
-      SBC B,227,X
-      AND B,229,X
-      BIT B,230,X
-      LDA B,231,X
-      STA B,232,X
-      EOR B,233,X
-      ADC B,234,X
-      ORA B,235,X
-      ADD B,236,X
+      SUB B 225,X
+      CMP B 226,X
+      SBC B 227,X
+      AND B 229,X
+      BIT B 230,X
+      LDA B 231,X
+      STA B 232,X
+      EOR B 233,X
+      ADC B 234,X
+      ORA B 235,X
+      ADD B 236,X
       LDX 239,X
       STX 240,X
-      SUB B,>$00F1
-      CMP B,>$00F2
-      SBC B,>$00F3
-      AND B,>$00F5
-      BIT B,>$00F6
-      LDA B,>$00F7
-      STA B,>$00F8
-      EOR B,>$00F9
-      ADC B,>$00FA
-      ORA B,>$00FB
-      ADD B,>$00FC
+      SUB B >$00F1
+      CMP B >$00F2
+      SBC B >$00F3
+      AND B >$00F5
+      BIT B >$00F6
+      LDA B >$00F7
+      STA B >$00F8
+      EOR B >$00F9
+      ADC B >$00FA
+      ORA B >$00FB
+      ADD B >$00FC
       LDX >$00FF
       STX $0100
