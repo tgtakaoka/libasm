05e555133c16499e618650f287219a0ff5ddb0c5
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-09-02 19:51:03 +0900

    [Arduino] Add all assembler/disassembler as examples

---
+++ examples/asm_z8000/asm_z8000.ino
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Tadashi G. Takaoka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <arduino_example.h>
+#include <asm_z8000.h>
+
+using libasm::arduino::AsmExample;
+using libasm::z8000::AsmZ8000;
+
+AsmZ8000 asmz8000;
+AsmExample example(asmz8000);
+
+void setup() {
+  Serial.begin(9600);
+  example.begin(Serial);
+}
+
+void loop() {
+  example.loop();
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// tab-width: 2
+// End:
+// vim: set ft=cpp et ts=2 sw=2:
