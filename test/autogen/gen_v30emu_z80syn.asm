127167abc297b2aacdd10b80b5a03c61d9a9e7eb
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2022-07-26 21:13:15 +0900

    [V30EMU] Add auto generated test source

---
+++ test/autogen/gen_v30emu_z80syn.asm
@@ -0,0 +1,24 @@
+; Copyright 2022 Tadashi G. Takaoka
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+        cpu     v30emu
+        z80syntax    on
+        org     0100h
+        include "gen_v30emu_z80syn.inc"
+        end
+
+; Local Variables:
+; mode: asm
+; End:
+; vim: set ft=asm:
