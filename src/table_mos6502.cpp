tgtakaoka/libasm

commit 5f40c2dfb062e9666b3d6332f0842e20cf372286

Refactor assembler/disassembler options much simpler

src/table_mos6502.cpp
@@ -756,11 +756,10 @@ Error TableMos6502::searchOpCode(InsnMos6502 &insn, StrBuffer &out) const {
 }
 
 void TableMos6502::reset() {
-    if (_cpu == nullptr)
-        setCpu(MOS6502);
-    useIndirectLong(true);
+    setCpu(MOS6502);
     setLongAccumulator(false);
     setLongIndex(false);
+    useIndirectLong(false);
 }
 
 bool TableMos6502::setCpu(CpuType cpuType) {
@@ -806,30 +805,30 @@ bool TableMos6502::setCpu(const char *cpu) {
 }
 
 bool TableMos6502::useIndirectLong(bool enable) {
-    if (enable && _cpu->cpuType() != W65C816) {
-        _useIndirectLong = false;
-        return false;
+    if (_cpu->cpuType() == W65C816) {
+        _useIndirectLong = enable;
+        return true;
     }
-    _useIndirectLong = enable;
-    return true;
+    _useIndirectLong = false;
+    return !enable;
 }
 
 bool TableMos6502::setLongAccumulator(bool on) {
-    if (on && _cpu->cpuType() != W65C816) {
-        _longAccumulator = false;
-        return false;
+    if (_cpu->cpuType() == W65C816) {
+        _longAccumulator = on;
+        return true;
     }
-    _longAccumulator = on;
-    return true;
+    _longAccumulator = false;
+    return !on;
 }
 
 bool TableMos6502::setLongIndex(bool on) {
-    if (on && _cpu->cpuType() != W65C816) {
-        _longIndex = false;
-        return false;
+    if (_cpu->cpuType() == W65C816) {
+        _longIndex = on;
+        return true;
     }
-    _longIndex = on;
-    return true;
+    _longIndex = false;
+    return !on;
 }
 
 bool TableMos6502::longImmediate(AddrMode mode) const {

cli/asm_commander.cpp
@@ -215,7 +215,7 @@ int AsmCommander::usage() {
             "  -h          : use lowe case letter for hexadecimal\n"
             "  -n          : output line number to list file\n"
             "  -v          : print progress verbosely\n"
-            "  -X<name>=<vale>\n"
+            "  --<name>=<vale>\n"
             "              : extra options (<type> [, <CPU>])\n",
             _prog_name, list.c_str());
     auto common = true;
@@ -307,15 +307,10 @@ int AsmCommander::parseArgs(int argc, const char **argv) {
             case 'v':
                 _verbose = true;
                 break;
-            case 'X':
-                if (*++opt) {
-                    if (parseOptionValue(opt) == 0)
-                        break;
-                } else if (++i < argc) {
-                    if (parseOptionValue(argv[i]) == 0)
-                        break;
-                }
-                fprintf(stderr, "-X requires option=value\n");
+            case '-':
+                if (parseOptionValue(++opt) == 0)
+                    break;
+                fprintf(stderr, "long option requires option=value\n");
                 return 1;
             default:
                 fprintf(stderr, "unknown option: %s\n", opt);

cli/dis_commander.cpp
@@ -165,7 +165,7 @@ int DisCommander::usage() {
             "  -h          : use lower case letter for hexadecimal\n"
             "  -u          : use upper case letter for output\n"
             "  -v          : print progress verbosely\n"
-            "  -X<name>=<vale>\n"
+            "  --<name>=<vale>\n"
             "              : extra options (<type> [, <CPU>])\n",
             _prog_name, cpuOption, list.c_str());
     auto common = true;
@@ -278,15 +278,10 @@ int DisCommander::parseArgs(int argc, const char **argv) {
                     }
                 }
                 break;
-            case 'X':
-                if (*++opt) {
-                    if (parseOptionValue(opt) == 0)
-                        break;
-                } else if (++i < argc) {
-                    if (parseOptionValue(argv[i]) == 0)
-                        break;
-                }
-                fprintf(stderr, "-X requires option=value\n");
+            case '-':
+                if (parseOptionValue(++opt) == 0)
+                    break;
+                fprintf(stderr, "long option requires option=value\n");
                 return 1;
             default:
                 fprintf(stderr, "unknown option: %s\n", opt);

driver/test/test_asm_formatter.cpp
@@ -139,7 +139,7 @@ void test_mc6809() {
             "        org   $abcd\n"
             "        cmpd  [$1234,y] ; indirect\n"
             "        include \"data/fdb.inc\"\n"
-            "        setdp $ff00\n",
+            "        setdp $ff\n",
             "       1/       0 :                            cpu   mc6809\n"
             "       2/       0 :                    * comment line\n"
             "       3/    ABCD :                            org   $abcd\n"
@@ -152,7 +152,7 @@ void test_mc6809() {
             "             ABE6 : 65 27\n"
             "(1)    4/    ABE8 : 61 2C 2F 20 00             fcb   'a, ',, '/, ' , 0\n"
             "(1)    5/    ABED : 41 27 C3 E1                fcb   'A', '', 'C'+$80, $80+'a\n"
-            "       6/    ABF1 :                            setdp $ff00\n");
+            "       6/    ABF1 :                            setdp $ff\n");
 }
 
 void test_mc6800() {

src/asm_base.cpp
@@ -35,7 +35,7 @@ Error Assembler::encode(const char *line, Insn &insn, SymbolTable *symtab) {
     if (symbol.size() == 0)
         return setError(scan, UNKNOWN_INSTRUCTION);
     insn.clearNameBuffer().text(symbol);
-    auto error = _pseudos.processPseudo(scan, insn, *this);
+    auto error = _pseudos.processPseudo(scan, insn, this);
     if (error != UNKNOWN_DIRECTIVE)
         return setError(error);
 

src/asm_base.h
@@ -35,7 +35,7 @@ class Assembler;
 struct OperandBase : public ErrorAt {};
 
 struct PseudoBase {
-    virtual Error processPseudo(StrScanner &scan, Insn &insn, Assembler &assembler) {
+    virtual Error processPseudo(StrScanner &scan, Insn &insn, Assembler *assembler) {
         return UNKNOWN_DIRECTIVE;
     }
 };

src/asm_cdp1802.cpp
@@ -38,15 +38,28 @@ struct AsmCdp1802::Operand : public OperandBase {
 AsmCdp1802::AsmCdp1802()
     : Assembler(_parser, TableCdp1802::TABLE, _pseudos, &_opt_useReg),
       _parser(_number, _comment, _symbol, _letter, _location),
+      _opt_useReg(this, &AsmCdp1802::setUseReg, OPT_BOOL_USE_REGISTER, OPT_DESC_USE_REGISTER,
+              _opt_smartBranch),
+      _opt_smartBranch(
+              this, &AsmCdp1802::setSmartBranch, OPT_BOOL_SMART_BRANCH, OPT_DESC_SMART_BRANCH),
       _pseudos() {
     reset();
 }
 
-AsmCdp1802::OptSmartBranch::OptSmartBranch(bool &var)
-    : BoolOption(OPT_BOOL_SMART_BRANCH, OPT_DESC_SMART_BRANCH, var) {}
+void AsmCdp1802::reset() {
+    setUseReg(false);
+    setSmartBranch(false);
+}
 
-AsmCdp1802::OptUseRegister::OptUseRegister(bool &var, const OptionBase &next)
-    : BoolOption(OPT_BOOL_USE_REGISTER, OPT_DESC_USE_REGISTER, var, next) {}
+Error AsmCdp1802::setUseReg(bool enable) {
+    _useReg = enable;
+    return OK;
+}
+
+Error AsmCdp1802::setSmartBranch(bool enable) {
+    _smartBranch = enable;
+    return OK;
+}
 
 static Config::uintptr_t page(Config::uintptr_t addr) {
     return addr & ~0xFF;

src/asm_cdp1802.h
@@ -29,7 +29,10 @@ public:
     AsmCdp1802();
 
     const ConfigBase &config() const override { return *this; }
-    void reset() override { _useReg = _smartBranch = false; }
+    void reset() override;
+
+    Error setUseReg(bool enable);
+    Error setSmartBranch(bool enable);
 
 private:
     ValueParser _parser;
@@ -39,20 +42,15 @@ private:
             return (scan[0] == '.' && scan[1] == '.') || endOfLine(scan);
         }
     } _comment;
-
     const DefaultSymbolParser _symbol;
     const IbmLetterParser _letter{/*prefix*/ 'T'};
     const AsteriskLocationParser _location;
-    PseudoBase _pseudos;
+    const BoolOption<AsmCdp1802> _opt_useReg;
+    const BoolOption<AsmCdp1802> _opt_smartBranch;
 
+    PseudoBase _pseudos;
     bool _useReg;
     bool _smartBranch;
-    const struct OptSmartBranch : public BoolOption {
-        OptSmartBranch(bool &var);
-    } _opt_smartBranch{_smartBranch};
-    const struct OptUseRegister : public BoolOption {
-        OptUseRegister(bool &var, const OptionBase &next);
-    } _opt_useReg{_useReg, _opt_smartBranch};
 
     struct Operand;
     Error parseOperand(StrScanner &scan, Operand &op) const;

commit af41ff3ec9de0bc62ac3a1df27cd5819d7690a33

[MOS6502] Fix can not switch CPU
This bug has introduced by 5f40c2dfb.

src/table_mos6502.cpp
@@ -755,8 +755,14 @@ Error TableMos6502::searchOpCode(InsnMos6502 &insn, StrBuffer &out) const {
     return insn.getError();
 }
 
+TableMos6502::TableMos6502() {
+    _cpu = nullptr;
+    reset();
+}
+
 void TableMos6502::reset() {
-    setCpu(MOS6502);
+    if (_cpu == nullptr)
+        setCpu(MOS6502);
     setLongAccumulator(false);
     setLongIndex(false);
     useIndirectLong(false);
@@ -801,28 +807,25 @@ bool TableMos6502::setCpu(const char *cpu) {
 }
 
 bool TableMos6502::useIndirectLong(bool enable) {
-    if (_cpu->cpuType() == W65C816) {
-        _useIndirectLong = enable;
+    _useIndirectLong = enable;
+    if (_cpu->cpuType() == W65C816)
         return true;
-    }
     _useIndirectLong = false;
     return !enable;
 }
 
 bool TableMos6502::setLongAccumulator(bool on) {
-    if (_cpu->cpuType() == W65C816) {
-        _longAccumulator = on;
+    _longAccumulator = on;
+    if (_cpu->cpuType() == W65C816)
         return true;
-    }
     _longAccumulator = false;
     return !on;
 }
 
 bool TableMos6502::setLongIndex(bool on) {
-    if (_cpu->cpuType() == W65C816) {
-        _longIndex = on;
+    _longIndex = on;
+    if (_cpu->cpuType() == W65C816)
         return true;
-    }
     _longIndex = false;
     return !on;
 }

src/table_mos6502.h
@@ -25,7 +25,7 @@ namespace libasm {
 namespace mos6502 {
 
 struct TableMos6502 : entry::Table {
-    TableMos6502() { reset(); }
+    TableMos6502();
     void reset();
 
     static TableMos6502 TABLE;

test/test_dis_mos6502.cpp
@@ -47,6 +47,7 @@ static bool w65c816() {
 static void set_up() {
     disassembler.reset();
     disassembler.setOption("relative", "no");
+    disassembler.setOption("indirect-long", "on");
 }
 
 static void tear_down() {