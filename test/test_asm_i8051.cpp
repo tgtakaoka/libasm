d0f81f829def8051b27bb9bf12dcaaece118188b
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2023-10-11 11:15:27 +0900

    [I8051] Cleanup instruction table

--- test/test_asm_i8051.cpp
+++ test/test_asm_i8051.cpp
@@ -168,6 +168,10 @@ static void test_indirect() {
     TEST("MOVC A,@A+DPTR", 0x93);
     TEST("MOVX A,@DPTR",   0xE0);
     TEST("MOVX @DPTR,A",   0xF0);
+    TEST("MOVX A,@R0",     0xE2);
+    TEST("MOVX A,@R1",     0xE3);
+    TEST("MOVX @R0,A",     0xF2);
+    TEST("MOVX @R1,A",     0xF3);
 
     TEST("INC @R0", 0x06);
     TEST("INC @R1", 0x07);
