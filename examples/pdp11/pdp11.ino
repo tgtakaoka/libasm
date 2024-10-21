a9be840858932aca0e15dd3f8a43ae6fbcd1df77
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-10-21 10:46:24 +0900

    [arduino] Add PDP-11 example sketch

---
+++ examples/pdp11/pdp11.ino
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 Tadashi G. Takaoka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <arduino_example.h>
+#include <asm_pdp11.h>
+#include <dis_pdp11.h>
+
+libasm::pdp11::AsmPdp11 asmpdp11;
+libasm::pdp11::DisPdp11 dispdp11;
+
+libasm::arduino::Example example(asmpdp11, dispdp11);
+
+void setup() {
+    Serial.begin(9600);
+    example.begin(Serial);
+}
+
+void loop() {
+    example.loop();
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// tab-width: 2
+// End:
+// vim: set ft=cpp et ts=2 sw=2:
