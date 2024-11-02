9c3a146b3a2eb8647b4709bd58d7c67425f74719
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-11-03 05:16:15 +0900

    [arduino] Fix DxCore package manager URL
    See https://github.com/SpenceKonde/megaTinyCore/issues/1059#issuecomment-1950286469

--- examples/install-arduino-cli.sh
+++ examples/install-arduino-cli.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-version=0.33.0
 export BINDIR=${HOME}/.local/bin
 echo "rm -f ${BINDIR}/arduino-cli"
+rm -f ${BINDIR}/arduino-cli
 
 which arduino-cli || {
-    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh -s ${version}
+    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
     hash -r
     }
 test -f ${HOME}/.arduino15/arduino-cli.yaml || arduino-cli config init
 
 arduino-cli config add board_manager.additional_urls https://mcudude.github.io/MightyCore/package_MCUdude_MightyCore_index.json
-arduino-cli config add board_manager.additional_urls https://drazzy.com/package_drazzy.com_index.json
+# arduino-cli config add board_manager.additional_urls http://drazzy.com/package_drazzy.com_index.json
+arduino-cli config add board_manager.additional_urls https://descartes.net/package_drazzy.com_index.json
 arduino-cli config add board_manager.additional_urls https://www.pjrc.com/teensy/package_teensy_index.json
 arduino-cli core update-index
 
