ed1b97b1d41eeea25bb7481522efa738aea06d6f
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2025-04-01 11:59:13 +0900

    [MOS6502] Cleanup Entry and Flags

--- src/entry_mos6502.h
+++ src/entry_mos6502.h
@@ -25,41 +25,30 @@
 namespace libasm {
 namespace mos6502 {
 
-namespace {
-static constexpr int opr1_gp = 0;
-static constexpr int indir_bp = 4;  // ()
-static constexpr int longi_bp = 5;  // []
-static constexpr int opr2_gp = 6;
-static constexpr int opr3_gp = 12;
-static constexpr uint16_t mode_gm = 0xF;
-static constexpr uint16_t indir_bm = (1 << indir_bp);
-static constexpr uint16_t longi_bm = (1 << longi_bp);
-}  // namespace
-
 enum AddrMode : uint8_t {
-    M_NONE = 0,             // none
-    I_FLAG = indir_bm,      // Indirect: (_)
-    L_FLAG = longi_bm,      // Indirect long: [_]
-    M_REGA = 1,             // Accumulator: A
-    M_REGX = 2,             // Index register: X
-    I_REGX = 2 | indir_bm,  // Indirect X: (X)
-    M_REGY = 3,             // Index register: Y
-    M_REGS = 4,             // Stack pointer: S
-    I_REGS = 4 | indir_bm,  // Stack indirect: (S)
+    M_NONE = 0,           // none
+    I_FLAG = 0x10,        // Indirect: (_)
+    L_FLAG = 0x20,        // Indirect long: [_]
+    M_REGA = 1,           // Accumulator: A
+    M_REGX = 2,           // Index register: X
+    I_REGX = 2 | I_FLAG,  // Indirect X: (X)
+    M_REGY = 3,           // Index register: Y
+    M_REGS = 4,           // Stack pointer: S
+    I_REGS = 4 | I_FLAG,  // Stack indirect: (S)
     // Memory addressing
-    M_IMA = 5,              // Immediate for A: #nn
-    M_IMX = 6,              // Immediate for X: #nn
-    M_IM8 = 7,              // Immediate byte: #nn
-    M_ABS = 8,              // Absolute: a16
-    I_ABS = 8 | indir_bm,   // Absolute indirect: (a16)
-    L_ABS = 8 | longi_bm,   // Absolute long indirect: [a16]
-    M_ABSL = 9,             // Absolute long: a24
-    M_DPG = 10,             // Direct page: d
-    I_DPG = 10 | indir_bm,  // Direct page indirect: (d)
-    L_DPG = 10 | longi_bm,  // Direct page long indirect: [d]
-    M_REL = 11,             // Relative: r8
-    M_LREL = 12,            // Relative long: r16
-    M_BANK = 13,            // Bank address: k8
+    M_IMA = 5,            // Immediate for A: #nn
+    M_IMX = 6,            // Immediate for X: #nn
+    M_IM8 = 7,            // Immediate byte: #nn
+    M_ABS = 8,            // Absolute: a16
+    I_ABS = 8 | I_FLAG,   // Absolute indirect: (a16)
+    L_ABS = 8 | L_FLAG,   // Absolute long indirect: [a16]
+    M_ABSL = 9,           // Absolute long: a24
+    M_DPG = 10,           // Direct page: d
+    I_DPG = 10 | I_FLAG,  // Direct page indirect: (d)
+    L_DPG = 10 | L_FLAG,  // Direct page long indirect: [d]
+    M_REL = 11,           // Relative: r8
+    M_LREL = 12,          // Relative long: r16
+    M_BANK = 13,          // Bank address: k8
     // Direct page indexed with X:           M_DPG,  M_REGX, M_NONE: d,X
     // Direct page indexed with Y:           M_DPG,  M_REGY, M_NONE: d,Y
     // Absolute indexed with X:              M_ABS,  M_REGX, M_NONE: a16,X
@@ -86,16 +75,28 @@ struct Entry final : entry::Base<Config::opcode_t> {
         uint16_t _attr;
 
         static constexpr Flags create(AddrMode opr1, AddrMode opr2, AddrMode opr3) {
-            return Flags{static_cast<uint16_t>((static_cast<uint16_t>(opr1) << opr1_gp) |
-                                               (static_cast<uint16_t>(opr2) << opr2_gp) |
-                                               (static_cast<uint16_t>(opr3) << opr3_gp))};
+            return Flags{static_cast<uint16_t>(((opr1 & mode1_gm) << mode1_gp) |
+                                               ((opr2 & mode2_gm) << mode2_gp) |
+                                               ((opr3 & mode3_gm) << mode3_gp))};
         }
 
-        AddrMode mode1() const {
-            return AddrMode((_attr >> opr1_gp) & (mode_gm | indir_bm | longi_bm));
-        }
-        AddrMode mode2() const { return AddrMode((_attr >> opr2_gp) & (mode_gm | indir_bm)); }
-        AddrMode mode3() const { return AddrMode((_attr >> opr3_gp) & mode_gm); }
+        AddrMode mode1() const { return AddrMode((_attr >> mode1_gp) & mode1_gm); }
+        AddrMode mode2() const { return AddrMode((_attr >> mode2_gp) & mode2_gm); }
+        AddrMode mode3() const { return AddrMode((_attr >> mode3_gp) & mode3_gm); }
+
+        static AddrMode baseMode(AddrMode mode) { return AddrMode(mode & basemode_gm); }
+        static AddrMode indirectFlags(AddrMode mode) { return AddrMode(mode & ~basemode_gm); }
+
+    private:
+        static constexpr int mode1_gp = 0;
+        static constexpr int indir_bp = 4;  // ()
+        static constexpr int longi_bp = 5;  // []
+        static constexpr int mode2_gp = 6;
+        static constexpr int mode3_gp = 11;
+        static constexpr uint16_t basemode_gm = 0x0F;
+        static constexpr uint16_t mode1_gm = 0x3F;  // mode|indir|longi
+        static constexpr uint16_t mode2_gm = 0x1F;  // mode|indir
+        static constexpr uint16_t mode3_gm = 0x0F;  // mode
     };
 
     constexpr Entry(Config::opcode_t opCode, Flags flags, const /* PROGMEM */ char *name_P)
@@ -103,10 +104,10 @@ struct Entry final : entry::Base<Config::opcode_t> {
 
     Flags readFlags() const { return Flags{pgm_read_word(&_flags_P._attr)}; }
 
-    static AddrMode baseMode(AddrMode mode) { return AddrMode(uint8_t(mode) & mode_gm); }
-    static AddrMode indirectFlags(AddrMode mode) { return AddrMode(uint8_t(mode) & ~mode_gm); }
-    static bool indirect(AddrMode mode) { return (uint8_t(mode) & indir_bm) != 0; }
-    static bool longIndirect(AddrMode mode) { return (uint8_t(mode) & longi_bm) != 0; }
+    static AddrMode baseMode(AddrMode mode) { return Flags::baseMode(mode); }
+    static AddrMode indirectFlags(AddrMode mode) { return Flags::indirectFlags(mode); }
+    static bool indirect(AddrMode mode) { return (mode & I_FLAG) != 0; }
+    static bool longIndirect(AddrMode mode) { return (mode & L_FLAG) != 0; }
 
 private:
     const Flags _flags_P;
