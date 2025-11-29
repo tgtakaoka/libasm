df5387ba519e9d7d308ed23e3fa79690fb0e2ce2
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2025-11-29 22:51:49 +0900

    [test] remove unused test sources

--- test/reference/mc68020.asm
+++
@@ -1,8 +0,0 @@
-        cpu     68020
-        ;; Wrong opcode
-        bfchg   d1{31:32}       ;<EAC1>07C0
-        bfclr   d1{31:32}       ;<ECC1>07C0
-        bfset   d1{31:32}       ;<EEC1>07C0
-        ;; Garbage in unused field
-        muls.l  d2,d7           ; 4C02<7800>
-        mulu.l  d2,d3           ; 4C02<3800>
