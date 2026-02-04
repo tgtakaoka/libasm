3ccc43bbd8d6ebeca55993da987902047ccaad3d
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2026-02-04 11:07:08 +0900

    Use perl instead of GNU grep

--- scripts/exract-common-text.sh
+++ scripts/exract-common-text.sh
@@ -5,13 +5,29 @@
 # - under src/ directory
 # - ../scripts/extract-common-text.sh
 
+# check if grep support -P (PCRE)
+grep -q -P x <<<"x" 2>/dev/null && PCRE=1 || PCRE=0
 
 function get_text {
-    grep -hPo '(?<=^extern const char )TEXT_[\w_]+' "$@"
+    if [[ ${PCRE} = 1 ]]; then
+        grep -hPo '(?<=^extern const char )TEXT_[\w_]+' "$@"
+    else
+        perl -ne 'print "$&\n" if /(?<=^extern const char )TEXT_[\w_]+/' "$@"
+    fi
 }
 
 function list_files {
-    for f in $(grep -lPow '(?<=^extern const char )'"$1" text_*.h); do
+    local -a list=()
+    if [[ ${PCRE} = 1 ]]; then
+        list=($(grep -lPow '(?<=^extern const char )'"$1" text_*.h))
+    else
+        list=($(perl -ne 'if (/(?<=^extern const char )\Q'"$1"'\E\b/) {
+               print "$ARGV\n";
+               close ARGV;
+         }' text_*.h))
+    fi
+    local f
+    for f in "${list[@]}"; do
         printf "%-16s" $f
     done
     echo
@@ -30,6 +46,6 @@ for f in text_*.h; do
 done
 
 for t in $(list_array "${texts[@]}" | uniq -d); do
-    printf "%-7s" "${t/TEXT_/}"
+    printf "%-12s" "${t/TEXT_/}"
     list_files "$t"
 done
--- scripts/make-index-array.sh
+++ scripts/make-index-array.sh
@@ -8,7 +8,14 @@
 # - Mark start and end of table.
 # - C-u M-x shell-command-on-region ../scripts/make-index-array.sh
 
-grep -Po '(TEXT_[A-Za-z0-9]+)' |
+# check if grep support -P (PCRE)
+grep -q -P x <<<"x" 2>/dev/null && PCRE=1 || PCRE=0
+
+if [[ ${PCRE} = 1 ]]; then
+    grep -Po '(TEXT_[A-Za-z0-9]+)'
+else
+    perl -ne 'print "$1\n" while /(TEXT_[A-Za-z0-9]+)/g'
+fi |
     sed 's/TEXT_null/@@@@_null/' |
     awk '{ printf("    %3d,  // %s\n", i++, $1); }' |
     sort -k3 -s |
